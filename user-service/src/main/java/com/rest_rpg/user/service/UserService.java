8d48c7d1e42de8f898deeba036909cae15b83e4b
MichalSulima1999 <michal.sulima1999@gmail.com>
2024-11-24 22:01:17 +0100

    Mailer integration

--- user-service/src/main/java/com/rest_rpg/user/service/UserService.java
+++ user-service/src/main/java/com/rest_rpg/user/service/UserService.java
@@ -1,26 +1,21 @@
 package com.rest_rpg.user.service;
 
+import com.rest_rpg.mailer_api.model.SendVerificationEmailEvent;
 import com.rest_rpg.user.api.model.Role;
 import com.rest_rpg.user.exception.AccountEmailExistsException;
 import com.rest_rpg.user.exception.AccountUsernameExistsException;
 import com.rest_rpg.user.exception.UserAlreadyVerifiedException;
-import com.rest_rpg.user.exception.VerificationEmailSendErrorException;
 import com.rest_rpg.user.model.User;
 import com.rest_rpg.user.model.dto.RegisterRequest;
 import com.rest_rpg.user.repository.UserRepository;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-
-import java.io.UnsupportedEncodingException;
 
 @Service
 @RequiredArgsConstructor
@@ -28,8 +23,8 @@ import java.io.UnsupportedEncodingException;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final JavaMailSender mailSender;
     private final PasswordEncoder passwordEncoder;
+    private final KafkaTemplate<String, SendVerificationEmailEvent> kafkaTemplate;
 
     public void register(@NotNull RegisterRequest request,
                          @NotNull HttpServletRequest servletRequest,
@@ -62,34 +57,9 @@ public class UserService {
     }
 
     private void sendVerificationEmail(@NotNull User user, @NotBlank String verificationURL) {
-        String toAddress = user.getEmail();
-        String fromAddress = "server@restrpg.com";
-        String senderName = "RPG";
-        String subject = "Please verify your registration";
-        String content = "Dear [[name]],<br>"
-                + "Please click the link below to verify your registration:<br>"
-                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
-                + "Thank you,<br>"
-                + "Rest RPG.";
-
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-
-        try {
-            helper.setFrom(fromAddress, senderName);
-            helper.setTo(toAddress);
-            helper.setSubject(subject);
-            content = content.replace("[[name]]", user.getUsername());
-            String verifyURL = verificationURL + "/user/verify?code=" + user.getVerificationCode();
-
-            content = content.replace("[[URL]]", verifyURL);
-
-            helper.setText(content, true);
-        } catch (MessagingException | UnsupportedEncodingException e) {
-            throw new VerificationEmailSendErrorException();
-        }
-
-        mailSender.send(message);
+        String verifyURL = verificationURL + "/user/verify?code=" + user.getVerificationCode();
+        SendVerificationEmailEvent event = new SendVerificationEmailEvent(user.getUsername(), user.getEmail(), verifyURL);
+        kafkaTemplate.send(SendVerificationEmailEvent.TOPIC_NAME, event);
     }
 
     private String getSiteURL(@NotNull HttpServletRequest request) {
